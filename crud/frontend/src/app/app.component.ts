74db153830a03b8d8bd93ace19620ae7aa287c3b
Yanaiara <yanaiarag@gmail.com>
2024-01-03 20:15:01 -0300

    feat: created header and added styling to the component

--- crud/frontend/src/app/app.component.ts
+++ crud/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from "./components/template/header/header.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
 @Component({
-  selector: 'app-root',  //seletor
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: 'app.component.html',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: 'app.component.html',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule]
 })
 export class AppComponent { }
