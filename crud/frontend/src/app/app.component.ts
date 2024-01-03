69203628894ee7c6512df09962a43d8a8420bb37
Yanaiara <yanaiarag@gmail.com>
2024-01-03 20:53:51 -0300

    feat: created footer and added styling to the component

--- crud/frontend/src/app/app.component.ts
+++ crud/frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from "./components/template/footer/footer.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent]
 })
 export class AppComponent { }
