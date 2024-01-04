91df9e64636ebefddf216d1da4d901db9c516734
Yanaiara <yanaiarag@gmail.com>
2024-01-03 21:29:26 -0300

    feat: created navigation, added styling

--- crud/frontend/src/app/app.component.ts
+++ crud/frontend/src/app/app.component.ts
@@ -4,10 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from "./components/template/footer/footer.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list"
+import { NavComponent } from "./components/template/nav/nav.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent, MatSidenavModule, MatListModule, NavComponent]
 })
 export class AppComponent { }
